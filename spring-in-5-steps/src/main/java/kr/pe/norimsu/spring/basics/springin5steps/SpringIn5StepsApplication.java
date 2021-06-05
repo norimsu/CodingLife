1a9384cc4537036dd230b4989a1ce57d0ad176b1
SANGHYUN LEE <norimsu@gmail.com>
2021-06-05 20:39:38 +0900

    Create Project for Spring Basic
    - at start.spring.io, generate with no dependency

---
+++ spring-in-5-steps/src/main/java/kr/pe/norimsu/spring/basics/springin5steps/SpringIn5StepsApplication.java
@@ -0,0 +1,13 @@
+package kr.pe.norimsu.spring.basics.springin5steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIn5StepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIn5StepsApplication.class, args);
+	}
+
+}
---
+++ spring-in-5-steps/src/test/java/kr/pe/norimsu/spring/basics/springin5steps/SpringIn5StepsApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.pe.norimsu.spring.basics.springin5steps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIn5StepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
