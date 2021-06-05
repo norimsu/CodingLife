52fcb9500ac130186b7fc32d80c5bbcb7352099d
SANGHYUN LEE <norimsu@gmail.com>
2021-06-06 00:35:59 +0900

    use QuickSortAlgorithm

--- spring-in-5-steps/src/main/java/kr/pe/norimsu/spring/basics/springin5steps/BinarySearchImpl.java
+++ spring-in-5-steps/src/main/java/kr/pe/norimsu/spring/basics/springin5steps/BinarySearchImpl.java
@@ -3,9 +3,9 @@ package kr.pe.norimsu.spring.basics.springin5steps;
 public class BinarySearchImpl {
 
 	public int binarySearch(int[] numbers, int numberToSearchFor) {
-		// Use Bubble Sort Algorithm
-		final BubbleSortAlgorithm bubbleSortAlgorithm = new BubbleSortAlgorithm();
-		final int[] sortedNumbers = bubbleSortAlgorithm.sort(numbers);
+		// Use Quick Sort Algorithm
+		final QuickSortAlgorithm quickSortAlgorithm = new QuickSortAlgorithm();
+		final int[] sortedNumbers = quickSortAlgorithm.sort(numbers);
 		// Search the array
 		return 3;
 	}
---
+++ spring-in-5-steps/src/main/java/kr/pe/norimsu/spring/basics/springin5steps/QuickSortAlgorithm.java
@@ -0,0 +1,10 @@
+package kr.pe.norimsu.spring.basics.springin5steps;
+
+public class QuickSortAlgorithm {
+
+	public int[] sort(int[] numbers) {
+		// Logic for Quick Sort
+		return numbers;
+	}
+
+}
