c7022b95896e49bcf854fd80af4dc430c88ea293
SANGHYUN LEE <norimsu@gmail.com>
2021-06-06 00:31:43 +0900

    use BubbleSortAlgorithm

--- spring-in-5-steps/src/main/java/kr/pe/norimsu/spring/basics/springin5steps/BinarySearchImpl.java
+++ spring-in-5-steps/src/main/java/kr/pe/norimsu/spring/basics/springin5steps/BinarySearchImpl.java
@@ -1,12 +1,11 @@
 package kr.pe.norimsu.spring.basics.springin5steps;
 
 public class BinarySearchImpl {
-	// Sorting an array
-	// Search the array
-	// Return the result
 
 	public int binarySearch(int[] numbers, int numberToSearchFor) {
-		// Implementing Sorting Logic
+		// Use Bubble Sort Algorithm
+		final BubbleSortAlgorithm bubbleSortAlgorithm = new BubbleSortAlgorithm();
+		final int[] sortedNumbers = bubbleSortAlgorithm.sort(numbers);
 		// Search the array
 		return 3;
 	}
---
+++ spring-in-5-steps/src/main/java/kr/pe/norimsu/spring/basics/springin5steps/BubbleSortAlgorithm.java
@@ -0,0 +1,9 @@
+package kr.pe.norimsu.spring.basics.springin5steps;
+
+public class BubbleSortAlgorithm {
+
+	public int[] sort(int[] numbers) {
+		// Logic for Bubble Sort
+		return numbers;
+	}
+}
