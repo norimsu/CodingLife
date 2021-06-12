10ebb545ccfd2d069f46e6052861a4c45d8e8a98
SANGHYUN LEE <norimsu@gmail.com>
2021-06-12 20:44:30 +0900

    use @Primary to avoid NoUniqueBeanDefinitionException with several candidates

--- spring-in-5-steps/src/main/java/kr/pe/norimsu/spring/basics/springin5steps/BubbleSortAlgorithm.java
+++ spring-in-5-steps/src/main/java/kr/pe/norimsu/spring/basics/springin5steps/BubbleSortAlgorithm.java
@@ -1,8 +1,10 @@
 package kr.pe.norimsu.spring.basics.springin5steps;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class BubbleSortAlgorithm implements SortAlgorithm {
 
 	public int[] sort(int[] numbers) {
