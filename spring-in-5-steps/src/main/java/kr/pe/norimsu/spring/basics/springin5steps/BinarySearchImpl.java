ad3df346842a553bdf4c31fdf0d5d3a6e679b8e3
SANGHYUN LEE <norimsu@gmail.com>
2021-06-12 23:54:56 +0900

    use variable name for Autowiring dependency

--- spring-in-5-steps/src/main/java/kr/pe/norimsu/spring/basics/springin5steps/BinarySearchImpl.java
+++ spring-in-5-steps/src/main/java/kr/pe/norimsu/spring/basics/springin5steps/BinarySearchImpl.java
@@ -6,22 +6,22 @@ import org.springframework.stereotype.Component;
 @Component
 public class BinarySearchImpl {
 
-//	@Autowired      // Setter Injection - No Setter & Constructor
-	private SortAlgorithm sortAlgorithm;
+	@Autowired      // Setter Injection - No Setter & Constructor
+	private SortAlgorithm bubbleSortAlgorithm; // use variable name
 
 //	//@Autowired    // Construction Injection via Constructor
 //	public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
 //		this.sortAlgorithm = sortAlgorithm;
 //	}
 
-	@Autowired      // Setter Injection with Setter
-	public void setSortAlgorithm(SortAlgorithm sortAlgorithm) {
-		this.sortAlgorithm = sortAlgorithm;
-	}
+//	@Autowired      // Setter Injection with Setter
+//	public void setSortAlgorithm(SortAlgorithm sortAlgorithm) {
+//		this.sortAlgorithm = sortAlgorithm;
+//	}
 
 	public int binarySearch(int[] numbers, int numberToSearchFor) {
-		final int[] sortedNumbers = sortAlgorithm.sort(numbers);
-		System.out.println("sortAlgorithm = " + sortAlgorithm);
+		final int[] sortedNumbers = bubbleSortAlgorithm.sort(numbers);
+		System.out.println("sortAlgorithm = " + bubbleSortAlgorithm);
 		// Search the array
 		return 3;
 	}
--- spring-in-5-steps/src/main/java/kr/pe/norimsu/spring/basics/springin5steps/BubbleSortAlgorithm.java
+++ spring-in-5-steps/src/main/java/kr/pe/norimsu/spring/basics/springin5steps/BubbleSortAlgorithm.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
+//@Primary
 public class BubbleSortAlgorithm implements SortAlgorithm {
 
 	public int[] sort(int[] numbers) {
