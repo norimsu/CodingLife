f8625ca4c15e49edd4f93c647f4cdfb4bf8e6796
SANGHYUN LEE <norimsu@gmail.com>
2021-06-18 18:46:25 +0900

    JUnit test with mockito

--- spring-in-5-steps/src/test/java/kr/pe/norimsu/spring/basics/springin5steps/cdi/SomeCdiBusinessTest.java
+++ spring-in-5-steps/src/test/java/kr/pe/norimsu/spring/basics/springin5steps/cdi/SomeCdiBusinessTest.java
@@ -1,23 +1,40 @@
 package kr.pe.norimsu.spring.basics.springin5steps.cdi;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = SpringIn5StepsCdiApplication.class)
+@ExtendWith(MockitoExtension.class)
 class SomeCdiBusinessTest {
 
-	@Autowired
+	//Inject Mock
+	@InjectMocks
 	SomeCdiBusiness business;
+
+	//Create Mock
+	@Mock
+	SomeCdiDao daoMock;
 
 	@Test
 	void basicScenario() {
-		// it depends on real DataSources.
-		assertEquals(100, business.findGreatest());
+		when(daoMock.getData()).thenReturn(new int[]{2, 4});
+		assertEquals(4, business.findGreatest());
+	}
+
+	@Test
+	void basicScenario_NoElements() {
+		when(daoMock.getData()).thenReturn(new int[]{});
+		assertEquals(Integer.MIN_VALUE, business.findGreatest());
+	}
+
+	@Test
+	void basicScenario_EqualElements() {
+		when(daoMock.getData()).thenReturn(new int[]{2, 2});
+		assertEquals(2, business.findGreatest());
 	}
 }
