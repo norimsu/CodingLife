f9ecac1998cb75c4596e5665b9ecbbf37013e457
SANGHYUN LEE <norimsu@gmail.com>
2021-06-18 16:27:17 +0900

    Create Project for Mockito Basic
    - at start.spring.io, generate with no dependency

---
+++ mockito-in-5-steps/src/main/java/kr/pe/norimsu/mockito/basics/mockitoin5steps/MockitoIn5StepsApplication.java
@@ -0,0 +1,13 @@
+package kr.pe.norimsu.mockito.basics.mockitoin5steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoIn5StepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockitoIn5StepsApplication.class, args);
+	}
+
+}
