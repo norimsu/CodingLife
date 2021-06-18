1a79a5c2dcc5636958d2793a7000194b8179c8ba
SANGHYUN LEE <norimsu@gmail.com>
2021-06-18 18:39:14 +0900

    JUnit test with spring-test

--- spring-in-5-steps/src/test/java/kr/pe/norimsu/spring/basics/springin5steps/basic/SpringIn5StepsBasicApplicationTests.java
+++ spring-in-5-steps/src/test/java/kr/pe/norimsu/spring/basics/springin5steps/basic/SpringIn5StepsBasicApplicationTests.java
@@ -1,9 +1,8 @@
 package kr.pe.norimsu.spring.basics.springin5steps.basic;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SpringIn5StepsBasicApplicationTests {
 
 	@Test
