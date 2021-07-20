56d3f1864747774eb5c7d1ce2ea89647a19c9f43
SANGHYUN LEE <norimsu@gmail.com>
2021-07-21 02:22:09 +0900

    refactor : rename variable

--- refactoring-java/src/main/java/kr/pe/norimsu/refactoring/Customer.java
+++ refactoring-java/src/main/java/kr/pe/norimsu/refactoring/Customer.java
@@ -58,28 +58,28 @@ public class Customer {
 
     /**
      * 대여 정보에 대한 대여료 계산
-     * @param each 대여 정보
+     * @param aRental 대여 정보
      * @return 대여료
      */
-    private double amountFor(Rental each) {
-        double thisAmount = 0;
-        switch (each.getMovie().getPriceCode()) {
+    private double amountFor(Rental aRental) {
+        double result = 0;
+        switch (aRental.getMovie().getPriceCode()) {
             case Movie.REGULAR:
-                thisAmount += 2;
-                if (each.getDaysRented() > 2) {
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                result += 2;
+                if (aRental.getDaysRented() > 2) {
+                    result += (aRental.getDaysRented() - 2) * 1.5;
                 }
                 break;
             case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
+                result += aRental.getDaysRented() * 3;
                 break;
             case Movie.CHILDREN:
-                thisAmount += 1.5;
-                if (each.getDaysRented() > 3) {
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                result += 1.5;
+                if (aRental.getDaysRented() > 3) {
+                    result += (aRental.getDaysRented() - 3) * 1.5;
                 }
                 break;
         }
-        return thisAmount;
+        return result;
     }
 }
