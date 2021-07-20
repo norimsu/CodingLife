1fbf0a7e7787b001029c51842342341bf7f54be6
SANGHYUN LEE <norimsu@gmail.com>
2021-07-21 02:36:35 +0900

    refactor : Extract Method

--- refactoring-java/src/main/java/kr/pe/norimsu/refactoring/Customer.java
+++ refactoring-java/src/main/java/kr/pe/norimsu/refactoring/Customer.java
@@ -31,13 +31,8 @@ public class Customer {
         while (rentals.hasMoreElements()) {
             Rental each = rentals.nextElement();
 
-            // 적립 포인트를 1 포인트 증가
-            frequentRenterPoints++;
-
-            // 최신물을 이틀 이상 대여하면 보너스 포인트 지급
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) {
-                frequentRenterPoints++;
-            }
+            // 경우에 따른 적립 포인트 지급 함수를 호출
+            frequentRenterPoints = getFrequentRenterPoints(frequentRenterPoints, each);
 
             // 이번에 대여하는 비디오 정보와 대여료를 출력
             result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getChange()) + "\n";
@@ -52,4 +47,20 @@ public class Customer {
         return result;
     }
 
+    /**
+     * 적립 포인트 계산
+     * @param frequentRenterPoints 포인트
+     * @param each 대여 정보
+     * @return 적립 포인트
+     */
+    private int getFrequentRenterPoints(int frequentRenterPoints, Rental each) {
+        // 적립 포인트를 1 포인트 증가
+        frequentRenterPoints++;
+
+        // 최신물을 이틀 이상 대여하면 보너스 포인트 지급
+        if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) {
+            frequentRenterPoints++;
+        }
+        return frequentRenterPoints;
+    }
 }
